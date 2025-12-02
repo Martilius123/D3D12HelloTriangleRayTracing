77d8ebaad32ce9a2d4e91501ef74219b57b0b2be
Aleksander-Sz <92821901+Aleksander-Sz@users.noreply.github.com>
2025-12-02 14:15:01 +0100

    fix, we used to calculate Specular incorrectly

--- MirrorDemoShader.hlsl
+++ MirrorDemoShader.hlsl
@@ -43,7 +43,7 @@ void ClosestHit_MirrorDemo(inout HitInfo payload, Attributes attrib)
     float3 incoming = WorldRayDirection();
     float3 viewDir = normalize(-incoming);
     
-    float3 reflectDir = reflect(-lightDir, hitNormal);
+    float3 reflectDir = reflect(-viewDir, hitNormal);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0f), 32.0f); // shininess 32
     if(payload.hopCount==0)
     {
